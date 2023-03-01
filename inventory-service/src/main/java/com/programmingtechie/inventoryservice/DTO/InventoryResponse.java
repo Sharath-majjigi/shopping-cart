9b78d09285fe689ec2afb1269b03dab9114e6cd0
Sharath <sharathholmes143@gmail.com>
2023-03-01 21:02:33 +0530

    order and inventory service communication using webflux

---
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/DTO/InventoryResponse.java
@@ -0,0 +1,16 @@
+package com.programmingtechie.inventoryservice.DTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponse {
+    public String skuCode;
+    public boolean isInStock;
+}
